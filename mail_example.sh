9ab15817138df6369b94ce77f011f04b044bb9f3
googkhan <gokhangndz@gmail.com>
2019-10-09 02:12:24 +0300

    branch2de ek commit durumu

--- mail_example.sh
+++ mail_example.sh
@@ -3,3 +3,4 @@ Recipient="gokhangndz@gmail.com"
 Subject="Greetings!"
 Message="Welcome to our site"
 `mail -s $Subject $Recipient <<< $Message`
+# This should be send messages but does not send now.
