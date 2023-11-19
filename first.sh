571071f0744c3d548c3b22b718d094042ca559d4
Gokhan Gunduz <gokhangndz@gmail.com>
2023-11-19 18:40:37 +0300

    Delete first.sh
    sorry for bad language :(

--- first.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Hello world niggas with attitude !!"
-
-echo "Printing with newline"
-echo -n "Printing without newline"
-echo -e "\nRemoving \t backlash \t characters\n"
-echo -n "Printing without newline"
-echo -n "Printing without newline"
-echo "\nRemoving \t backlash \t characters\n"
